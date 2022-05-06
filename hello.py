18be6d41b3802a16878dbe28c4ac734c0e6113a7
Jieun Park <diwoon95@gmail.com>
2022-05-06 13:30:02 +0900

    second task

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
 
-print("hello")
 
-#description? 
+print("hello")
 
 #To go back to normal mode, press 'esc' on anyhwere 
 
+print("first task")
 
 for i in range (1,11):
 	if i%3 == 0:
 		print("year")
 	else:
 		print(i)
+print("second task")
+
+for j in range (1, 21):
+  if j%3 == 0:
+    print("year")
+  elif j%5 == 0 :
+    print("dream")
+  else:
+    print(j)
